ab2f6d3a958c8d89f12abf19f0131ba71021133b
Sam <Sam@DESKTOP-9N5HGK2>
2021-10-25 15:34:37 -0700

    10/25/2021 uploading classes from computer

---
+++ src/CreateNewLinksTestCases.java
@@ -0,0 +1,11 @@
+import org.junit.*;
+
+public class CreateNewLinksTestCases {
+	@Test
+	public void nameLinkTest() {
+		// jUnit test that tests if new links can accept user inputs
+		// takes in new link with name: Lwsd, link: lwsd.org, no username, no passsword
+		// test is successful if method returns link with these perimeters
+	}
+
+}
