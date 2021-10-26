f45fd9dccd34386c3e2f14ec6c08cbebea2bf263
Thys Vanderschoot <thysvanderschoot@gmail.com>
2021-10-26 09:26:59 -0700

    Added addInfo, importLinks, createDefaultFile, and comments for all

--- src/FileManager.java
+++ src/FileManager.java
@@ -11,13 +11,34 @@ public class FileManager {
 	
 	// Adds a link with the characteristics Name, Link, Username, and Password as
 	// given by the parameters
-	public static boolean addLink(String name, String link, String use, String pass)
+	
+	// Returns true if there was a link with "name" already, and false if it was a new
+	// name
+	public static boolean addLink(String name, String link, String user, String pass)
 	{
 		return false;
 	}
 	
+	// Changes the username and password areas of the website named "name"
+	
+	// Returns true if the username and password were input, and false if the name
+	// submitted does not appear in the text file
+	public static boolean addInfo(String name, String user, String pass)
+	{
+		return false;
+	}
+	
+	// Reads through the file and imports all links in the file to QuickLinks, stored in
+	// a HashMap
 	private static void importLinks()
 	{
-		
+		return;
+	}
+	
+	// Checks if there is a file in the folder already, and if there is not it will create
+	// a file and load it with the default links
+	private static void createDefaultFile()
+	{
+		return;
 	}
 }
