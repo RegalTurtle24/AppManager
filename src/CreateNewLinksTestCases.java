d104d4fba5d66a8b34e314510554402fb61ffbbe
s-SAFERREIRA <s-SAFERREIRA@487M089717.lwsd.k12wa.org>
2021-10-26 15:18:43 -0700

    10/26/2021 I can't create a working comparison test

--- src/CreateNewLinksTestCases.java
+++ src/CreateNewLinksTestCases.java
@@ -4,7 +4,8 @@ public class CreateNewLinksTestCases {
 	@Test
 	public void nameLinkTest() {
 		QuickLink link = new QuickLink("lwsd", "lwsd.org", null, null);
-		Assert.assertEquals(CreateNewLinks.createNewLink("lwsd", "lwsd.org"));
+		QuickLink createdLink = CreateNewLinks.createNewLink("lwsd", "lwsd.org");
+		//Assert.assertEquals(createdLink);
 		// jUnit test that tests if new links can accept user inputs
 		// takes in new link with name: Lwsd, link: lwsd.org, no username, no passsword
 		// test is successful if method returns link with these perimeters
