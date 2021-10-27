a1310ca089eaf876e95640d4383ab31f6fadc5b0
s-ADIXIT <s-ADIXIT@487M089672.lwsd.k12wa.org>
2021-10-27 07:34:12 -0700

    first commit - added runner and quicklinks, tests in quicklinks too.

---
+++ src/QuickLinks.java
@@ -0,0 +1,9 @@
+// make and save links
+public class QuickLinks {
+	//i'm not sure if we're doing encryption, 
+	//so username and password will be plaintext for now.
+	
+	public static QuickLinks (String newAddress, String uname, String passwd) {
+		
+	}
+}
---
+++ src/QuickLinksTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import java.lang.StringBuilder;
+//tests QuickLinks.
+
+class QuickLinksTest {
+
+	@Test
+	void test() {
+		StringBuilder tester = new StringBuilder(16);
+		
+		String password = "lol lmao";
+	}
+
+}
