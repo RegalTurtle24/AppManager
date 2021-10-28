daf58c821787ab787cac6e82f6c785716e6d347f
Thys Vanderschoot <thysvanderschoot@gmail.com>
2021-10-28 08:03:44 -0700

    Skeleton API for new 2 class plan 10/28/21 803

--- src/FileManager.java
+++ src/FileManager.java
@@ -1,20 +1,33 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class FileManager {
-	private static final int LINES_PER_LINK = 4;
-	
-	// Constructor calls ImportLinks and CreateDefaultFile if needed
-	public FileManager()
+	private static final int LINES_PER_SET = 4;
+		
+	// Purpose: Gets the next set of LINES_PER_SET lines and returns them in an ArrayList to
+	// LinkManager
+	// Input: None
+	// Output: ArrayList of the next LINES_PER_SET lines
+	public static ArrayList<String> getNextSet()
 	{
-		createDefaultFile();
+		return null;
 	}
 	
-	public static getNext
-	
-	// Checks if there is a file in the folder already, and if there is not it will create
+	// Purpose: Checks if there is a file in the folder already, and if there is not it will create
 	// a file and load it with the default links
-	private static void createDefaultFile()
+	// Input: None
+	// Output: If there is no file, create file
+	public static void createDefaultFile()
 	{
 		return;
+	}
+	
+	// Purpose: Write a new set of information at the bottom of the file or where 1st parameter
+	// (name) is found
+	// Input: ArrayList of parameters to be added
+	// Output: Returns false if no data was overridden, true if any data was changed
+	public static boolean writeToFile(ArrayList<String> parameters)
+	{
+		return false;
 	}
 }
--- src/LinkManager.java
+++ src/LinkManager.java
@@ -8,28 +8,38 @@ public class LinkManager {
 		links = new HashMap<String, QuickLinks>();
 	}
 	
-	// Adds a link with the characteristics Name, Link, Username, and Password as
+	// Purpose: Adds a link with the characteristics Name, Link, Username, and Password as
 	// given by the parameters
-	
-	// Returns true if there was a link with "name" already, and false if it was a new
+	// Input: QuickLinks
+	// Output: Returns true if there was a link with name already, and false if it was a new
 	// name
-	public static boolean addLink(String name, String link, String user, String pass)
+	public boolean addLink(QuickLinks link)
 	{
 		return false;
 	}
 	
-	// Changes the username and password areas of the website named "name"
-	
-	// Returns true if the username and password were input, and false if the name
+	// Purpose: Changes the username and password areas of the website named "name"
+	// Input: Name of website, username for site, and password for site
+	// Output: Returns true if the username and password were input, and false if the name
 	// submitted does not appear in the text file
-	public static boolean addCredentials(String name, String user, String pass)
+	public boolean addCredentials(String name, String user, String pass)
 	{
 		return false;
 	}
 	
-	// Returns the HashMap of QuickLinks
-	public static HashMap<String, QuickLink> getHashMap()
+	// Purpose: Returns the HashMap of QuickLinks
+	// Input: None
+	// Output: HashMap of links
+	public HashMap<String, QuickLinks> getHashMap()
 	{
 		return links;
+	}
+	
+	// Purpose: Returns a QuickLink in the HashMap based on a String name
+	// Input: Name of link
+	// Output: QuickLinks if it was found, null if not
+	public QuickLinks getLink(String name)
+	{
+		return null;
 	}
 }
