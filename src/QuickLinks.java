39c7f1d740f2ffa3f4762a9c62552903d71f7dda
s-ADIXIT <s-ADIXIT@487M089672.lwsd.k12wa.org>
2021-10-28 07:57:11 -0700

    finished QuickLinks ((:

--- src/QuickLinks.java
+++ src/QuickLinks.java
@@ -2,8 +2,50 @@
 public class QuickLinks {
 	//i'm not sure if we're doing encryption, 
 	//so username and password will be plaintext for now.
+	private String username;
+	private String password;
+	private String url;
+	private String name;
 	
-	public static QuickLinks (String newAddress, String uname, String passwd) {
-		
+	public QuickLinks (String username, 
+					   String password, 
+					   String url, 
+					   String name) {
+		this.username = username;
+		this.password = password;
+		this.url = url;
+		this.name = name;
 	}
+	
+	public String getPassword () {
+		return password;
+	}
+	
+	public String getUrl() {
+		return url;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setUsername(String newUsername) {
+		username = newUsername;
+	}
+	
+	public void setPassword(String newPassword) {
+		password = newPassword;
+	}
+	
+	public void setUrl(String newUrl) {
+		url = newUrl;
+	}
+	
+	public void setName(String newName) {
+		name = newName;
+	}	
 }
--- src/QuickLinksTest.java
+++ src/QuickLinksTest.java
@@ -2,15 +2,52 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 import java.lang.StringBuilder;
-//tests QuickLinks.
 
-class QuickLinksTest {
+//tests the QuickLinks class.
 
-	@Test
-	void test() {
-		StringBuilder tester = new StringBuilder(16);
+public class QuickLinksTest {
+
+	//the other get methods are the same
+	//so i'm just making one test method
+	//for all of the get methods  ((: <3
+	@Test	
+	void testGetUsername() {
+		int length = 16;
+		StringBuilder tester = new StringBuilder(length);
 		
-		String password = "lol lmao";
+		for (int i = 0; i < length; i++) {
+			tester.append((char) (Math.random() * 94) + 20);
+		}
+		
+		String username = tester.toString();
+		System.out.println(username);
+		
+		QuickLinks test = new QuickLinks(username, "", "", "");
+		
+		assertTrue(test.getUsername() == username);
 	}
-
+	
+	
+	//the other set methods are the same
+	//so i'm just making one test method
+	//for all of the set methods  ((: <3
+	@Test
+	void testSetUsername() {
+		int length = 16;
+		StringBuilder tester = new StringBuilder(length);
+		
+		for (int i = 0; i < length; i++) {
+			tester.append((char) (Math.random() * 94) + 20);
+		}
+		
+		String username = tester.toString();
+		System.out.println("\n" + username);
+		
+		QuickLinks test = new QuickLinks("ilovebees", "", "", "");
+		System.out.println(test.getUsername());
+		
+		test.setUsername(username);
+		
+		assertTrue(test.getUsername() == username);
+	}
 }
