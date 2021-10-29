fbec77356cd4783e2a73f1aeb845bb39b3304580
s-LYING <s-LYING@487M089658.lwsd.k12wa.org>
2021-10-29 08:20:04 -0700

    working on tests and GUI methids

--- src/GUI.java
+++ src/GUI.java
@@ -9,32 +9,33 @@ public class GUI {
 		
 		JFrame f=new JFrame();  
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		// creating buttons and adding actionlistener manually (will be done automatically later)
-		JButton b1=new JButton("1");   
+		visuals(f);
+			
+	}  
+	
+	public static void visuals(JFrame f) {
+		// creating buttons and adding actionlistener manually (will be done automatically later in another helper class)
+		JButton b1=new JButton("1");  
 		b1.addActionListener(new ActionListener(){  
 		    public void actionPerformed(ActionEvent e){  
 		    		System.out.println("lol");
 		    } 
 		});
 		
-	    JButton b2=new JButton("2");    
-	    JButton b3=new JButton("3");    
-	    JButton b4=new JButton("4");    
-	    JButton b5=new JButton("5");    
-	    JButton b6=new JButton("6");  
-	    JButton b7=new JButton("new link");    
-
-   
-	     // adding buttons to the frame manually (will be done automatically later)       
-	    f.add(b1); f.add(b2); f.add(b3);  
-	    f.add(b4); f.add(b5); f.add(b6); f.add(b7);
-	  
-	    // setting grid layout of 3 rows and 3 columns    
-	    f.setLayout(new GridLayout(4,3, 10, 10));    
-	    f.setSize(1370,730);    
-	    f.setVisible(true); 	
-		
-
-	}  
+		JButton b2=new JButton("2");    
+		JButton b3=new JButton("3");    
+		JButton b4=new JButton("4");    
+		JButton b5=new JButton("5");    
+		JButton b6=new JButton("6");  
+		JButton b7=new JButton("new link");    
+		   
+		// adding buttons to the frame manually (will be done automatically later)       
+		f.add(b1); f.add(b2); f.add(b3);  
+		f.add(b4); f.add(b5); f.add(b6); f.add(b7);
+		  
+		// setting grid layout of 3 rows and 3 columns    
+		f.setLayout(new GridLayout(4,3, 10, 10));    
+		f.setSize(1370,730);    
+		f.setVisible(true); 
+	}
 }
--- src/GUItest.java
+++ src/GUItest.java
@@ -1,9 +1,22 @@
 import static org.junit.jupiter.api.Assertions.*;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 import org.junit.Test;
 
 public class GUItest {
 	@Test
-	void temp() {
+	void buttonPressed() {
+		GUI gui = new GUI();
+		JFrame f = new JFrame();
+		gui.visuals(f);
+		JButton test = new JButton("test");
+		if((test.getModel()).isPressed()) {
+	        System.out.println("the button is pressed");
+		}
 		fail("Not implemented yet");
 	}
+	
+
 }
