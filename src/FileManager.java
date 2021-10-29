126b0a5870badbf7dbc30acb397d0d38bc039a5d
Thys Vanderschoot <thysvanderschoot@gmail.com>
2021-10-29 08:52:03 -0700

    getNextSet implementation without tests 10/29/21 851

--- src/FileManager.java
+++ src/FileManager.java
@@ -1,18 +1,35 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Scanner;
 
 public class FileManager {
 	private static final int LINES_PER_SET = 4;
-	private static File file;
-		
+	private static Scanner sc;
+	private static File info;
+	
 	// Purpose: Gets the next set of LINES_PER_SET lines and returns them in an ArrayList to
 	// LinkManager
 	// Input: None
 	// Output: ArrayList of the next LINES_PER_SET lines
 	public static ArrayList<String> getNextSet()
 	{
-		return null;
+		if(info == null) {
+			info = new File("infoFile.txt");
+		}
+		if(sc == null) {
+			sc = new Scanner("infoFile");
+		}
+		ArrayList<String> set = new ArrayList<String>();
+		for(int i = 0 ; i < LINES_PER_SET ; i++) {
+			if (sc.hasNext()) {
+				set.add(sc.nextLine());
+			} else {
+				return null;
+			}
+		}
+		return set;
 	}
 	
 	// Purpose: Checks if there is a file in the folder already, and if there is not it will create
