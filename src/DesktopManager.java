cfd7f5a67f9bba8846d48c1ffc2c92b233e90170
s-MSUBOTIC <s-MSUBOTIC@487M089667.lwsd.k12wa.org>
2021-10-29 08:10:08 -0700

    skeleton to import quicklinks

--- src/DesktopManager.java
+++ src/DesktopManager.java
@@ -1,4 +1,15 @@
 
 public class DesktopManager {
-
+	public static void openSkywardGrading() {
+		return;
+	}
+	public static void openSkywardAttendance() {
+		
+	}
+	public static void openTeamsAssignments() {
+		
+	}
+	public static void openCustumQuicklink() {
+		
+	}
 }
