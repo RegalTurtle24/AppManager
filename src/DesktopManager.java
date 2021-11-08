a769f939f8091a022d56c18022f855c78267b915
s-MSUBOTIC <s-MSUBOTIC@lwsd.org>
2021-11-08 13:40:39 -0800

    test file made and refactored methods

--- src/DesktopManager.java
+++ src/DesktopManager.java
@@ -1,15 +1,31 @@
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DesktopManager {
+	
+	public static void openLink(QuickLinks customLink) {
+		
+	}
+	
+	public static void loginSkyward(QuickLinks skyward) {
+		
+	}
+	
 	public static void openSkywardGrading(QuickLinks skywardLink) {
-		
+		openLink(skywardLink);
 	}
+	
+	
 	public static void openSkywardAttendance(QuickLinks skywardLink) {
-		
+		openLink(skywardLink);
+
 	}
-	public static void openTeamsAssignments(QuickLinks teamsLink) {
+	
 		
-	}
-	public static void loginCustomQuicklink(QuickLinks cutomLink) {
-		
+	public static void loginCustomQuicklink(QuickLinks customLink) {
+		openLink(customLink);
+
 	}
 }
