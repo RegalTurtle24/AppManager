820802f609780c8abade46d1b2451f1d9d5215a7
s-ADIXIT <s-ADIXIT@487M089672.lwsd.k12wa.org>
2021-11-10 07:47:09 -0800

    removed unwanted dependencies in runner.

--- src/Runner.java
+++ src/Runner.java
@@ -1,9 +1,3 @@
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import javax.swing.*;
-
 public class Runner {
 	public static void main (String[] args) {
 		GUI gui = new GUI();
