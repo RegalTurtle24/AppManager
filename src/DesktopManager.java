ce287aabd4d7c6d805accba65e79ca980582b256
marko-subotic <marko.subotic@outlook.com>
2021-11-12 23:34:46 -0800

    adding comments and opening the gradebook method in progress

--- src/DesktopManager.java
+++ src/DesktopManager.java
@@ -3,32 +3,72 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import static org.openqa.selenium.support.locators.RelativeLocator.with;
+import java.util.List;
 
+
+//This entire class uses Selenium, a library for automating browsers.
+// The Selenium website and library can be found here: https://www.selenium.dev/
 public class DesktopManager {
 
 	private WebDriver driver;
+	
+	//Initializes the webdriver and finds where chromedriver is in the local library
 	public DesktopManager() {
 		System.setProperty("webdriver.chrome.driver", ".\\libs\\chromedriver.exe");
 		driver = new ChromeDriver(); 
 	}
+	
+	//This opens a single link
 	public void openLink(QuickLinks customLink) {
 		driver.get(customLink.getUrl());
 	}
 	
+	//opens skyward login and enters the credentials after finding the html ids for each login box
 	public void loginSkyward(QuickLinks skyward) {
 		driver.get(skyward.getUrl());
+		System.out.println(driver.getWindowHandle());
 		WebElement loginUser = driver.findElement(By.id("login"));
 		loginUser.sendKeys(skyward.getUsername());
 		WebElement loginPass = driver.findElement(By.id("password"));
 		loginPass.sendKeys(skyward.getPassword()+ Keys.ENTER);
 	}
 	
-	public void openSkywardGrading(QuickLinks skywardLink) {
-		loginSkyward(skywardLink);
-		WebElement home = driver.findElement(By.className("first selected"));
-		WebElement gradebook = driver.findElement(with(By.className("sf_navMenuItem")).below(home));
+	
+	//logs into skyward and opens the student gradebook
+	public void openSkywardGrading(QuickLinks skywardLink) throws InterruptedException {
+		
+		//stores the original window id to be able to change to an alternate window later
+		String originalWindow = driver.getWindowHandle();
+		loginSkyward(skywardLink);	
+		
+		//waits until a new window has been created
+		while(driver.getWindowHandles().size()==1) {
+			Thread.sleep(50);
+		}
+		//loops through to a new window (where the skyward pop-up will be
+		for (String windowHandle : driver.getWindowHandles()) {
+		    if(!originalWindow.contentEquals(windowHandle)) {
+		        driver.switchTo().window(windowHandle);
+		        break;
+		    }
+		}
+//		loops until the page loads and finds the menu webelement, then I create the list and choose
+//		the wanted index
+		WebElement menu;
+		while(true) {
+			try {
+				 menu = driver.findElement(By.id("sf_navMenu"));
+					System.out.println("hi");
+
+				break;
+			} catch(org.openqa.selenium.NoSuchElementException noSuchElementException) {
+				Thread.sleep(50);
+			}
+		}
+		List<WebElement> menuList = menu.findElements(By.tagName("li"));;
+		WebElement gradebook =  menuList.get(1);
 		gradebook.click();
+		System.out.println(gradebook.isEnabled());
 	}
 	
 	
@@ -37,7 +77,9 @@ public class DesktopManager {
 
 	}
 	
-		
+
+//	This opens the login and uses finding by id to login, most sites have a common
+//	name system for login: this is applicable for Pearson and AP
 	public void loginCustomQuicklink(QuickLinks customLink) {
 		openLink(customLink);
 		WebElement username = driver.findElement(By.id("username"));
@@ -47,12 +89,14 @@ public class DesktopManager {
 
 	}
 	
-	
+//	This is a method on its own to ensure modularity if something changes, I can fix this
+//	method and the user interface will work for this class
 	public void loginPearson(QuickLinks pearsonLink) {
 		loginCustomQuicklink(pearsonLink);
 	}
 	
-	
+//	This is a method on its own to ensure modularity if something changes, I can fix this
+//	method and the user interface will work for this class
 	public void loginAPClassroom(QuickLinks APLink) {
 		loginCustomQuicklink(APLink);
 	}
--- src/DesktopManagerTests.java
+++ src/DesktopManagerTests.java
@@ -9,11 +9,25 @@ public class DesktopManagerTests {
 	public void openBrowser() {
 		QuickLinks test = new QuickLinks("yes", "yes", "https://lwsd.org", "lwsd website");
 		runner.openLink(test);
-		assertTrue("this did not open lwsd.org", true);
+		assertTrue("this shouldn't fail", true);
 	}
-//	public static void loginSkyward() {
-//		QuickLinks test = new QuickLinks("yes", "yes", "https://www2.saas.wa-k12.net/scripts/cgiip.exe/WService=wlkwashs71/fwemnu01.w", "lwsd website");
-//		DesktopManager.loginSkyward(test);
-//		assertTrue("opened skyward", true);
-//	}
+	
+	@Test
+	public void loginSkyward() {
+		QuickLinks test = new QuickLinks("subotmar000", "DodgersH@veWon20", "https://www2.saas.wa-k12.net/scripts/cgiip.exe/WService=wlkwashs71/fwemnu01.w", "lwsd website");
+		runner.loginSkyward(test);
+		assertTrue("this shouldn't fail", true);
+	}
+	@Test
+	public void gradingSkyward() {
+		try{
+			QuickLinks test = new QuickLinks("subotmar000", "DodgersH@veWon20", "https://www2.saas.wa-k12.net/scripts/cgiip.exe/WService=wlkwashs71/fwemnu01.w", "lwsd website");
+		runner.openSkywardGrading(test);
+		assertTrue("this shouldn't fail", true);
+		} catch(InterruptedException interruptedException){
+			
+		} finally {
+			
+		}
+	}
 }
